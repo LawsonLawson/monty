256c486b6e02dea34660a4a6686613f34fec8127
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 13:45:28 +0000

    added function prototype for mul opcode function

--- monty.h
+++ monty.h
@@ -98,6 +98,7 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void div_op(stack_t **stack, unsigned int line_number);
+void mul_op(stack_t **stack, unsigned int line_number);
 
 /* parser and command exec. functions */
 void push_to_stack(stack_t **stack, unsigned int line_number);
