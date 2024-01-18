96afd36b2ee5e5deefde78e2fcc8623de90f4a7c
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 13:05:41 +0000

    added the implementation of the sub function in the source file

--- stack_operations.c
+++ stack_operations.c
@@ -188,3 +188,26 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+ * sub - Subtracts the top element of the stack form the second top element.
+ *
+ * @stack: A pointer to the head of the stack.
+ * @line_number: The line of the instructon in the Monty file.
+ *
+ * Description: This function subtracts the top element of the stack from
+ * the second top element. If the stack contains less than two elements,
+ * it prints an error message and exits with EXIT_FAILURE. The result is
+ * stored in the second top element of the stack, and the element is removed.
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (list_size(monty) < 2)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);/* Remove the top element */
+}
