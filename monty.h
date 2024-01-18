4db60841c2b07d5b60ac7ca36c087bc37c8be1d3
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 12:47:45 +0000

    added function prototype for nop opcode function

--- monty.h
+++ monty.h
@@ -95,6 +95,7 @@ void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
 /* parser and command exec. functions */
 void push_to_stack(stack_t **stack, unsigned int line_number);
