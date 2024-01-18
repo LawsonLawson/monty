02b4d23150417cc6bf775621d052bfedc7f7953a
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 12:35:36 +0000

    updated the instruction_t array to include the add opcode

--- monty_utils.c
+++ monty_utils.c
@@ -71,7 +71,7 @@ void execute_command(char *command)
 
 	/* an array of instruction_t struct with opcode-function pairs */
 	instruction_t instructions[] = {
-		{"push", push_to_stack}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}
+		{"push", push_to_stack}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}
 	};
 
 	/* Loop through the instructions array to find a matching opcode */
