c354424759325fd29999e5c07454db4e8755ec6e
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 13:04:57 +0000

    added function prototype for sub opcode function

--- monty.h
+++ monty.h
@@ -96,6 +96,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 
 /* parser and command exec. functions */
 void push_to_stack(stack_t **stack, unsigned int line_number);
