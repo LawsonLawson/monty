f0529fb454bfac261a1eba55f32d117bbc4ce1cf
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 13:05:18 +0000

    updated the instruction_t array to include the sub opcode

--- monty_utils.c
+++ monty_utils.c
@@ -71,7 +71,7 @@ void execute_command(char *command)
 
 	/* an array of instruction_t struct with opcode-function pairs */
 	instruction_t instructions[] = {
-		{"push", push_to_stack}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}
+		{"push", push_to_stack}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}, {"sub", sub}
 	};
 
 	/* Loop through the instructions array to find a matching opcode */
