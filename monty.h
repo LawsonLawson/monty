2829846fdb540d48440364e3e1ae55693b9e92c6
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 14:44:10 +0000

    added function prototype for pstr opcode function

--- monty.h
+++ monty.h
@@ -101,6 +101,7 @@ void div_op(stack_t **stack, unsigned int line_number);
 void mul_op(stack_t **stack, unsigned int line_number);
 void mod_op(stack_t **stack, unsigned int line_number);
 void pchar_op(stack_t **stack, unsigned int line_number);
+void pstr_op(stack_t **stack, unsigned int line_number);
 
 /* parser and command exec. functions */
 void push_to_stack(stack_t **stack, unsigned int line_number);
