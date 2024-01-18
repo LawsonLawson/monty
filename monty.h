de29d55765ae081b4f4391374fa2a5f0c45d565f
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 11:04:52 +0000

    added function prototype for pop in header file

--- monty.h
+++ monty.h
@@ -92,6 +92,7 @@ typedef struct instruction_s
 void push(stack_t **stack, int data);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 /* parser and command exec. functions */
 void push_to_stack(stack_t **stack, unsigned int line_number);
