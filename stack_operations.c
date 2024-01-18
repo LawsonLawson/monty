d11b973c527c5104d8440f7dfd7f95ba602e4c46
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 10:36:53 +0000

    added the implementation of the pint function to source file

--- stack_operations.c
+++ stack_operations.c
@@ -68,3 +68,23 @@ void pall(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 
 	fflush(stdout);
 }
+
+/**
+ * pint - Prints the value at the top of the stack.
+ *
+ * @stack: A pointer to the head of the stack.
+ * @line_number: The line in the Monty file where the pint opcode appears.
+ *
+ * Description: This function prints the value at the top of the stack and
+ * exits with EXIT_FAILURE if the stack is empty.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (list_empty(monty))
+	{
+		fprintf(stderr, "L%u: can't pint, stack empry\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
