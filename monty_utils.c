e2c58de037119d6daf97209c8982bb1809cc12b2
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 13:31:54 +0000

    updated the instruction_t array to include the div opcode

--- monty_utils.c
+++ monty_utils.c
@@ -71,7 +71,7 @@ void execute_command(char *command)
 
 	/* an array of instruction_t struct with opcode-function pairs */
 	instruction_t instructions[] = {
-		{"push", push_to_stack}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}, {"sub", sub}
+		{"push", push_to_stack}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}, {"sub", sub}, {"div", div_op}
 	};
 
 	/* Loop through the instructions array to find a matching opcode */
