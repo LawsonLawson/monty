de20175f62f217683244e71dc42c2541683045fb
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 15:28:38 +0000

    added implementation of rotl function to source file

--- stack_operations.c
+++ stack_operations.c
@@ -365,3 +365,38 @@ void pstr_op(stack_t **stack, unsigned int line_number)
 	_putchar('\n');
 	(void)line_number;/* perhaps __unused__ att would surfice */
 }
+
+
+/**
+ * rotl - Rotates the stack to the top.
+ *
+ * @stack: A pointer to the head of the stack.
+ * @line_number: The line number of the instruction in the Monty file.
+ *
+ * Description: This function rotates the stack to the top.
+ * The top element becomes the last one, and the second top element becomes the
+ * first one. If the stack contains less than two elements, it doesn't change
+ * the stack.
+ */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (list_size(monty) < 2)
+	{
+        /* If stack contains less than 2 elements, don't change the stack. */
+        return;
+	}
+	temp = *stack;
+	/* Traverse to the last element */
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	/* Adjust pointers for rotation */
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;/* Set new last element's next to NULL */
+	(*stack)->prev = NULL;
+}
