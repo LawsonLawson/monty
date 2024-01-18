4932ea0746c231c900f4a59cebda4f2e3685df3b
Lawson Pascal Israel <isreallawson@outlook.com>
2024-01-18 13:31:36 +0000

    added function prototype for div opcode function

--- monty.h
+++ monty.h
@@ -97,6 +97,7 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void div_op(stack_t **stack, unsigned int line_number);
 
 /* parser and command exec. functions */
 void push_to_stack(stack_t **stack, unsigned int line_number);
